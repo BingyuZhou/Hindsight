2ed37b2ddf7729ba89a6da1d40f1d8d2cccf0628
bingyu <zbyjeff@gmail.com>
2018-07-14 16:34:38 +0200

    update her

--- DQN/her.py
+++ DQN/her.py
@@ -36,32 +36,34 @@ def her(buffer, method, n, reward_fun):
         else:
             her_ind = buffer.end_index
 
-        for t in range(n):
-            selected_rows = random.sample(range(n), k)
+        future_p = 1.0 - 1.0 / (1.0 + k)
 
-            selected_ind = her_ind - 1 - np.array(selected_rows)
-            selected_ind = selected_ind.astype(np.float)
+        selected_rows = random.sample(range(n), int(n * future_p))
 
-            selected_ind[selected_ind < -0.1] += buffer.replay_buffer_size
+        selected_ind = her_ind - 1 - np.array(selected_rows)
+        selected_ind = selected_ind.astype(np.float)
 
-            her_goals = buffer.buffer[selected_ind.astype(int), 2 * n +
-                                      1:3 * n + 1]
-            for goal in her_goals:
-                replay_buffer_row = np.mod(
-                    her_ind - t - 1 + buffer.replay_buffer_size,
-                    buffer.replay_buffer_size)
+        selected_ind[selected_ind < -0.1] += buffer.replay_buffer_size
 
-                s = buffer.buffer[int(replay_buffer_row), 0:n]
-                a = buffer.buffer[int(replay_buffer_row), 2 * n]
-                s_next = buffer.buffer[int(replay_buffer_row), 2 * n +
-                                       1:3 * n + 1]
+        her_goals = buffer.buffer[selected_ind.astype(int), 2 * n + 1:3 * n +
+                                  1]
+        buffer.buffer[selected_ind.astype(int), n:2 * n] = her_goals
+        buffer.buffer[selected_ind.astype(int), -1] = 0
+        # for goal in her_goals:
+        #     replay_buffer_row = np.mod(
+        #         her_ind - t - 1 + buffer.replay_buffer_size,
+        #         buffer.replay_buffer_size)
 
-                reward_her = reward_fun(s_next, goal)
+        #     s = buffer.buffer[int(replay_buffer_row), 0:n]
+        #     a = buffer.buffer[int(replay_buffer_row), 2 * n]
+        #     s_next = buffer.buffer[int(replay_buffer_row), 2 * n + 1:3 * n + 1]
 
-                buffer.add(s, goal, a, s_next, reward_her)
+        #     reward_her = reward_fun(s_next, goal)
+
+        #     buffer.add(s, goal, a, s_next, reward_her)
         return buffer
 
     if (method == 'future'):
-        return _her_future(buffer, 4, n, reward_fun)
+        return _her_future(buffer, 1, n, reward_fun)
     elif (method == 'final'):
         return _her_final(buffer, n, reward_fun)
